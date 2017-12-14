ac6673ba1a903edebd1006775680be356ce5ef99
Reece Hudson <reecehudson10@gmail.com>
2017-12-13 21:11:09 -0600

    improve on withScrollBottomLoad()
    * use 3rd party <BottomScrollListener />
    * rid legacy "pagination" residue
    * rid jquery as dep
    *

--- src/decorators/withScrollBottomLoad.js
+++ src/decorators/withScrollBottomLoad.js
@@ -2,75 +2,35 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import $ from "jquery"
-
-/**
- *  1. listen on 'scroll'
- *  2. once we get to the zone near the bottom, stop listening on 'scroll'
- *  3. load more data
- *  4. wait for new data to come in
- *  5. make sure the new data has been rendered so we are out of the zone
- *  6. repeat -> 1
- */
-
- const zoneHeight = 100
- const waitForRenderTime = 1000 // 1 sec ? idk
+import BottomScrollListener from "react-bottom-scroll-listener"
 
 function withScrollBottomLoad(Wrapped) {
   class ScrollBottomLoadWrapper extends React.Component {
     constructor(props) {
       super(props);
-      this.onScroll = this.handleScroll.bind(this)
-      this.scrollEvOn = true
-      this.pagination = 1
-    }
-
-    componentDidMount() {
-      window.addEventListener('scroll', this.onScroll);
-    }
-
-    componentWillUnmount() {
-      window.removeEventListener('scroll', this.onScroll);
+      this.boundLoad = this.load.bind(this)
+      // TODO: make load() no-op if isLoading === true
     }
 
     load() {
-      let { activeIcon, load, pagination } = this.props
-
-      load( false, activeIcon, this.onLoadReqComplete.bind(this))
-    }
-
-    // 4.
-    onLoadReqComplete() {
-      // 5. how do we know the new data has been rendered ?
-      //    for now we wait just about enough for react to render it
-      window.setTimeout(() => {
-        this.scrollEvOn = true // 6.
-      }, waitForRenderTime)
-    }
-
-    handleScroll() {
-      if($(window).scrollTop() + $(window).height() > $(document).height() - zoneHeight) { // previously:  ($.fn.scrollTop.call(window) + $.fn.height.call(window) > $.fn.height.call(document) - zoneHeight)
-        if (this.scrollEvOn) { // 2.
-          this.scrollEvOn = false
-          this.load() // 3.
-        }
-      }
+      let { activeIcon, load } = this.props
+      load( false, activeIcon)
     }
 
     render() {
-      const passedProps = this.props
       return (
-        <div>
-          <Wrapped {...passedProps}/>
-        </div>
-      )
+      <BottomScrollListener
+        onBottom={this.boundLoad}
+        offset={200}
+        debounce={200}>
+        <Wrapped {... this.props}/>
+      </BottomScrollListener> )
     }
   }
 
   ScrollBottomLoadWrapper.propTypes = {
     activeIcon: PropTypes.string.isRequired,
     load: PropTypes.func.isRequired
-    // pagination: PropTypes.number
   }
 
   return ScrollBottomLoadWrapper
