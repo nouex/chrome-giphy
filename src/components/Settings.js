7f1bb4956cb071a4dc1b5e3ba221b7f67c88fcb1
Reece Hudson <reecehudson10@gmail.com>
2017-12-14 20:44:12 -0600

    use bootstrap-react components

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,6 +3,7 @@
 import React from "react"
 import PropTypes from 'prop-types';
 import actions from "../actions"
+import { Form, FormGroup, FormControl, ControlLabel, Col } from "react-bootstrap"
 
 /**
  * PROBLEM: reducers/settings.js requires dispatch(), how do we access dispatch?
@@ -39,61 +40,74 @@ class Settings extends React.Component {
           {settings} = this.props
 
     return (
-      <form>
-        <label htmlFor="rating-input">Rating
-          <select onChange={createModifier("rating", targetValueSelector)}
-                  id="rating-input"
-                  value={settings.rating}>
-            <option value="Y">Y</option>
-            <option value="G">G</option>
-            <option value="PG">PG</option>
-            <option value="PG-13">PG-13</option>
-            <option value="R">R</option>
-          </select>
-        </label>
-        <label htmlFor="limit-input">Request Limit
-          <input id="limit-input" type="number" value={settings.value}
-                 onChange={createModifier("limit", targetValueSelector)} />
-        </label>
-        <label htmlFor="lang-input">Lang
-          <select onChange={createModifier("lang", targetValueSelector)}
-                  id="lang-input"
-                  value={settings.lang} >
-           <option value="en">English</option>
-           <option value="es">Spanish</option>
-           <option value="pt">Portuguese</option>
-           <option value="id">Indonesian</option>
-           <option value="fr">French</option>
-           <option value="ar">Arabic</option>
-           <option value="tr">Turkish</option>
-           <option value="th">Thai</option>
-           <option value="vi">Vietnamese</option>
-           <option value="de">German</option>
-           <option value="it">Italian</option>
-           <option value="ja">Japanese</option>
-           <option value="zh-Cn">Chinese Simplified</option>
-           <option value="zh-TW">Chinese Traditional</option>
-           <option value="ru">Russian</option>
-           <option value="ko">Korean</option>
-           <option value="pl">Polish</option>
-           <option value="nl">Dutch</option>
-           <option value="ro">Romanian</option>
-           <option value="hu">Hungarian</option>
-           <option value="sv">Swedish</option>
-           <option value="cs">Czech</option>
-           <option value="hi">Hindi</option>
-           <option value="bn">Bengali</option>
-           <option value="da">Danish</option>
-           <option value="fa">Farsi</option>
-           <option value="tl">Filipino</option>
-           <option value="fi">Finnish</option>
-           <option value="iw">Hebrew</option>
-           <option value="ms">Malay</option>
-           <option value="no">Norwegian</option>
-           <option value="uk">Ukrainian</option>
-          </select>
-        </label>
-      </form>
+      <Form horizontal className="px-4 py-2">
+        <FormGroup controlId="rating-input">
+          <ControlLabel>Rating</ControlLabel>
+          <FormControl
+            componentClass={
+              () => (
+                <select onChange={createModifier("rating", targetValueSelector)}
+                        id="rating-input"
+                        value={settings.rating}>
+                  <option value="Y">Y</option>
+                  <option value="G">G</option>
+                  <option value="PG">PG</option>
+                  <option value="PG-13">PG-13</option>
+                  <option value="R">R</option>
+                </select>)
+              } />
+        </FormGroup>
+        <FormGroup controlId="limit-input">
+          <ControlLabel>Size Per Request</ControlLabel>
+          <FormControl type="number" value={settings.value}
+                       onChange={createModifier("limit", targetValueSelector)}
+                       onSubmit={() => {/* avoid the default FIXME */}} />
+        </FormGroup>
+        <FormGroup controlId="lang-input">
+          <ControlLabel>Language</ControlLabel>
+          <FormControl
+            componentClass={
+              () => (
+                <select onChange={createModifier("lang", targetValueSelector)}
+                        id="lang-input"
+                        value={settings.lang} >
+                 <option value="en">English</option>
+                 <option value="es">Spanish</option>
+                 <option value="pt">Portuguese</option>
+                 <option value="id">Indonesian</option>
+                 <option value="fr">French</option>
+                 <option value="ar">Arabic</option>
+                 <option value="tr">Turkish</option>
+                 <option value="th">Thai</option>
+                 <option value="vi">Vietnamese</option>
+                 <option value="de">German</option>
+                 <option value="it">Italian</option>
+                 <option value="ja">Japanese</option>
+                 <option value="zh-Cn">Chinese Simplified</option>
+                 <option value="zh-TW">Chinese Traditional</option>
+                 <option value="ru">Russian</option>
+                 <option value="ko">Korean</option>
+                 <option value="pl">Polish</option>
+                 <option value="nl">Dutch</option>
+                 <option value="ro">Romanian</option>
+                 <option value="hu">Hungarian</option>
+                 <option value="sv">Swedish</option>
+                 <option value="cs">Czech</option>
+                 <option value="hi">Hindi</option>
+                 <option value="bn">Bengali</option>
+                 <option value="da">Danish</option>
+                 <option value="fa">Farsi</option>
+                 <option value="tl">Filipino</option>
+                 <option value="fi">Finnish</option>
+                 <option value="iw">Hebrew</option>
+                 <option value="ms">Malay</option>
+                 <option value="no">Norwegian</option>
+                 <option value="uk">Ukrainian</option>
+                </select>
+              )
+            }/>
+        </FormGroup>
+      </Form>
     )
 
     function targetValueSelector({target: {value}}) {
