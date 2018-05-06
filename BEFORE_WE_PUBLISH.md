8288801143648b0890a67f691da356782bd8ca4c
Reece Hudson <reecehudson10@gmail.com>
2018-05-06 16:30:54 -0600

    stylize refresh btn

--- BEFORE_WE_PUBLISH.md
+++ BEFORE_WE_PUBLISH.md
@@ -1,5 +1,5 @@
 1. get rid of test and dev warnings
-2. refresh: stylize refresh btn
+2. refresh: stylize refresh btn ✅
 3. settings: add default size per request
 4. fix arrow navigation
 5. copy image to clipboar?  if not then copy url
--- src/components/Refresh.js
+++ src/components/Refresh.js
@@ -1,4 +1,6 @@
+import { Button } from "react-bootstrap"
+
 const React = require('react');
-const ReloadBtn = ({onClick}) => <h4 onClick={onClick}>refresh</h4>
+const ReloadBtn = ({onClick}) => <div className="px-4 py-2"><Button bsStyle="primary" onClick={onClick}>New</Button></div>
 
 export default ReloadBtn
